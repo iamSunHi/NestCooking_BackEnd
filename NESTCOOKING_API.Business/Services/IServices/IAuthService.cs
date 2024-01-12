cd78e10151f63b1821383d3b252eab66e7b721cf
NhatHuy <doan.nhathuy.03@gmail.com>
2024-01-12 12:29:31 +0700

    Revert "Revert "Add AuthController to sign in and sign up instead of UserController""
    This reverts commit 8915ff4ba60c673345b863cec21e10958355f560.

---
+++ NESTCOOKING_API.Business/Services/IServices/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using NESTCOOKING_API.Business.DTOs;
+
+namespace NESTCOOKING_API.Business.Services.IServices
+{
+	public interface IAuthService
+	{
+		Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO);
+		Task<string> Register(RegistrationRequestDTO registrationRequestDTO);
+	}
+}
--- NESTCOOKING_API.Business/Services/IServices/IUserService.cs
+++ NESTCOOKING_API.Business/Services/IServices/IUserService.cs
@@ -4,7 +4,5 @@ namespace NESTCOOKING_API.Business.Services.IServices
 {
 	public interface IUserService
 	{
-		Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO);
-		Task<string> Register(RegistrationRequestDTO registrationRequestDTO);
 	}
 }
