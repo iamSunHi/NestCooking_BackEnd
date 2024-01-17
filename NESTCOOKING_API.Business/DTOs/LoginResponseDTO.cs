9aadf0cd0b15d67e692dc4fc64c5334361a57e63
NhatHuy <doan.nhathuy.03@gmail.com>
2024-01-17 21:35:08 +0700

    Fix JsonPropertyName of AccessToken

--- NESTCOOKING_API.Business/DTOs/LoginResponseDTO.cs
+++ NESTCOOKING_API.Business/DTOs/LoginResponseDTO.cs
@@ -1,7 +1,10 @@
-﻿namespace NESTCOOKING_API.Business.DTOs
+﻿using System.Text.Json.Serialization;
+
+namespace NESTCOOKING_API.Business.DTOs
 {
 	public class LoginResponseDTO
 	{
+		[JsonPropertyName("access_token")]
 		public string AccessToken { get; set; }
     }
 }
