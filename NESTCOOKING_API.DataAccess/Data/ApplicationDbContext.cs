3905c7786c4c130064e2c81d37cc17b3b3ba0d31
NhatHuy <doan.nhathuy.03@gmail.com>
2024-01-30 21:04:11 +0700

    Remove unused tables in DbSet for Db.

--- NESTCOOKING_API.DataAccess/Data/ApplicationDbContext.cs
+++ NESTCOOKING_API.DataAccess/Data/ApplicationDbContext.cs
@@ -12,24 +12,7 @@ namespace NESTCOOKING_API.DataAccess.Data
 		{
 		}
 
-		public DbSet<Booking> Bookings { get; set; }
-		public DbSet<Category> Categories { get; set; }
-		public DbSet<Instructor> Instructors { get; set; }
-		public DbSet<Comment> Comments { get; set; }
-		public DbSet<CommentHierarchy> CommentHierarchies { get; set; }
-		public DbSet<CommentReaction> CommentReactions { get; set; }
-		public DbSet<Ingredient> Ingredients { get; set; }
-		public DbSet<Message> Messages { get; set; }
-		public DbSet<Notification> Notifications { get; set; }
-		public DbSet<Payment> Payments { get; set; }
-		public DbSet<Recipe> Recipes { get; set; }
-		public DbSet<PurchasedRecipe> PurchasedRecipes { get; set; }
-		public DbSet<Reaction> Reactions { get; set; }
-		public DbSet<Report> Reports { get; set; }
-		public DbSet<RequestToBecomeChef> RequestsToBecomeChef { get; set; }
-		public DbSet<Response> Responses { get; set; }
 		public DbSet<User> Users { get; set; }
-		public DbSet<UserConnection> UserConnections { get; set; }
 
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
