936a6137ae356a153d8fdfcdc235b0c43806444e
NhatHuy <doan.nhathuy.03@gmail.com>
2024-02-27 08:06:14 +0700

    Fix bug when update database.

--- NESTCOOKING_API.DataAccess/Models/RecipeReaction.cs
+++ NESTCOOKING_API.DataAccess/Models/RecipeReaction.cs
@@ -1,19 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace NESTCOOKING_API.DataAccess.Models
 {
-    public class RecipeReaction
+	public class RecipeReaction
     {
         [Key]
         public int Id { get; set; }
-        public User User { get; set; } = null!;
-        public Recipe Recipe { get; set; } = null!;
-        public Reaction Reaction { get; set; } = null!;
+		public string UserId { get; set; } = null!;
+		public User User { get; set; }
+		public string RecipeId { get; set; } = null!;
+		public Recipe Recipe { get; set; }
+		public string ReactionId { get; set; } = null!;
+		public Reaction Reaction { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
     }
