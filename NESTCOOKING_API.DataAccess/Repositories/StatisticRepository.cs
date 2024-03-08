7299bd95610480d0670a5b689ab5518b19d5b059
NhatHuy <doan.nhathuy.03@gmail.com>
2024-03-08 09:29:54 +0700

    sprint 3: hotfix: fix get violation in statistic background service

--- NESTCOOKING_API.DataAccess/Repositories/StatisticRepository.cs
+++ NESTCOOKING_API.DataAccess/Repositories/StatisticRepository.cs
@@ -6,74 +6,74 @@ using NESTCOOKING_API.Utility;
 
 namespace NESTCOOKING_API.DataAccess.Repositories
 {
-	public class StatisticRepository : IStatisticRepository
-	{
-		private readonly IServiceScopeFactory _scopeFactory;
+    public class StatisticRepository : IStatisticRepository
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
 
-		public StatisticRepository(IServiceScopeFactory scopeFactory)
-		{
-			_scopeFactory = scopeFactory;
-		}
+        public StatisticRepository(IServiceScopeFactory scopeFactory)
+        {
+            _scopeFactory = scopeFactory;
+        }
 
-		public async Task CreateNewDataIfNewDate(DateOnly currentDate)
-		{
-			using var scope = _scopeFactory.CreateScope();
-			var _context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-			if (_context.UserStatistics.OrderByDescending(us => us.Date).First().Date != currentDate)
-			{
-				var totalViolation = _context.Reports.Select(r => r.Status == StaticDetails.ActionStatus_ACCEPTED).Count();
-				await _context.ViolationStatistics.AddAsync(new() { Date = currentDate, TotalOfViolation = totalViolation });
+        public async Task CreateNewDataIfNewDate(DateOnly currentDate)
+        {
+            using var scope = _scopeFactory.CreateScope();
+            var _context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            if (_context.UserStatistics.OrderByDescending(us => us.Date).First().Date != currentDate)
+            {
+                var totalViolation = 0;
+                await _context.ViolationStatistics.AddAsync(new() { Date = currentDate, TotalOfViolation = totalViolation });
 
-				var totalRevenue = 0;
-				await _context.RevenueStatistics.AddAsync(new() { Date = currentDate, Revenue = totalRevenue });
+                var totalRevenue = 0;
+                await _context.RevenueStatistics.AddAsync(new() { Date = currentDate, Revenue = totalRevenue });
 
-				var totalBooking = 0;
-				await _context.BookingStatistics.AddAsync(new() { Date = currentDate, TotalOfBooking = totalBooking });
+                var totalBooking = 0;
+                await _context.BookingStatistics.AddAsync(new() { Date = currentDate, TotalOfBooking = totalBooking });
 
-				var totalUser = _context.Users.Count();
-				await _context.UserStatistics.AddAsync(new() { Date = currentDate, TotalOfUser = totalUser });
+                var totalUser = _context.Users.Count();
+                await _context.UserStatistics.AddAsync(new() { Date = currentDate, TotalOfUser = totalUser });
 
-				var roleChefId = (await _context.Roles.FirstAsync(r => r.Name == StaticDetails.Role_Chef)).Id;
-				var totalChef = _context.Users.Select(u => u.RoleId == roleChefId).Count();
-				await _context.ChefStatistics.AddAsync(new() { Date = currentDate, TotalOfChef = totalChef });
+                var roleChefId = (await _context.Roles.FirstAsync(r => r.Name == StaticDetails.Role_Chef)).Id;
+                var totalChef = _context.Users.Select(u => u.RoleId == roleChefId).Count();
+                await _context.ChefStatistics.AddAsync(new() { Date = currentDate, TotalOfChef = totalChef });
 
-				await _context.SaveChangesAsync();
-			}
-		}
+                await _context.SaveChangesAsync();
+            }
+        }
 
-		public async Task UpdateAllStatistics()
-		{
-			using var scope = _scopeFactory.CreateScope();
-			var _context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-			var currentDate = DateTime.UtcNow.Date;
-			var currentDateOnly = DateOnly.FromDateTime(currentDate);
-			var userListFromDb = _context.Users.ToList();
+        public async Task UpdateAllStatistics()
+        {
+            using var scope = _scopeFactory.CreateScope();
+            var _context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            var currentDate = DateTime.UtcNow.Date;
+            var currentDateOnly = DateOnly.FromDateTime(currentDate);
+            var userListFromDb = _context.Users.ToList();
 
-			var totalNewUser = userListFromDb.Where(u => u.CreatedAt.Date == currentDate.Date).Count();
-			var totalUser = userListFromDb.Count();
-			var currentUserStatistic = await _context.UserStatistics.FirstAsync(us => us.Date == currentDateOnly);
-			currentUserStatistic.NumberOfNewUser = totalNewUser;
-			currentUserStatistic.TotalOfUser = totalUser;
-			_context.UserStatistics.Update(currentUserStatistic);
+            var totalNewUser = userListFromDb.Where(u => u.CreatedAt.Date == currentDate.Date).Count();
+            var totalUser = userListFromDb.Count();
+            var currentUserStatistic = await _context.UserStatistics.FirstAsync(us => us.Date == currentDateOnly);
+            currentUserStatistic.NumberOfNewUser = totalNewUser;
+            currentUserStatistic.TotalOfUser = totalUser;
+            _context.UserStatistics.Update(currentUserStatistic);
 
-			var roleChef = await _context.Roles.FirstAsync(r => r.Name == StaticDetails.Role_Chef);
-			var totalNewChef = userListFromDb.Where(u => u.RoleId == roleChef.Id && u.CreatedAt.Date == currentDate.Date).Count();
-			var totalChef = userListFromDb.Where(u => u.RoleId == roleChef.Id).Count();
-			var currentChefStatistic = await _context.ChefStatistics.FirstAsync(us => us.Date == currentDateOnly);
-			currentChefStatistic.NumberOfNewChef = totalNewChef;
-			currentChefStatistic.TotalOfChef = totalChef;
-			_context.ChefStatistics.Update(currentChefStatistic);
+            var roleChef = await _context.Roles.FirstAsync(r => r.Name == StaticDetails.Role_Chef);
+            var totalNewChef = userListFromDb.Where(u => u.RoleId == roleChef.Id && u.CreatedAt.Date == currentDate.Date).Count();
+            var totalChef = userListFromDb.Where(u => u.RoleId == roleChef.Id).Count();
+            var currentChefStatistic = await _context.ChefStatistics.FirstAsync(us => us.Date == currentDateOnly);
+            currentChefStatistic.NumberOfNewChef = totalNewChef;
+            currentChefStatistic.TotalOfChef = totalChef;
+            _context.ChefStatistics.Update(currentChefStatistic);
 
-			var reportListFromDb = _context.Reports.ToList();
-			var totalViolation = reportListFromDb.Where(r => r.Status == StaticDetails.ActionStatus_COMPLETED).Count();
-			var currentViolationStatistic = await _context.ViolationStatistics.FirstAsync(us => us.Date == currentDateOnly);
-			currentViolationStatistic.TotalOfViolation = totalViolation;
-			_context.ViolationStatistics.Update(currentViolationStatistic);
+            var reportListFromDb = await _context.Reports.ToListAsync();
+            var totalViolation = reportListFromDb.Where(r => r.Status == StaticDetails.ActionStatus_ACCEPTED).Count();
+            var currentViolationStatistic = await _context.ViolationStatistics.FirstAsync(us => us.Date == currentDateOnly);
+            currentViolationStatistic.TotalOfViolation = totalViolation;
+            _context.ViolationStatistics.Update(currentViolationStatistic);
 
-			// Revenue
-			// Booking
+            // Revenue
+            // Booking
 
-			await _context.SaveChangesAsync();
-		}
-	}
+            await _context.SaveChangesAsync();
+        }
+    }
 }
