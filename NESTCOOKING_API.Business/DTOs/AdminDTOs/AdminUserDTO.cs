d4e919c2300797ffd2045f650e4daa9d76361823
NhatHuy <doan.nhathuy.03@gmail.com>
2024-03-20 20:47:20 +0700

    Add related services for admin to manage users.

---
+++ NESTCOOKING_API.Business/DTOs/AdminDTOs/AdminUserDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace NESTCOOKING_API.Business.DTOs.AdminDTOs
+{
+	public class AdminUserDTO
+	{
+		public string Id { get; set; } = null!;
+		public string UserName { get; set; } = null!;
+		public string Email { get; set; } = null!;
+		public bool EmailConfirmed { get; set; }
+		public string FirstName { get; set; } = null!;
+		public string LastName { get; set; } = null!;
+		public string? AvatarUrl { get; set; }
+		public string? LockOutEnd { get; set; }
+	}
+}
--- NESTCOOKING_API.Business/Services/UserService.cs
+++ NESTCOOKING_API.Business/Services/UserService.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Identity;
+using NESTCOOKING_API.Business.DTOs.AdminDTOs;
 using NESTCOOKING_API.Business.DTOs.UserDTOs;
 using NESTCOOKING_API.Business.Exceptions;
 using NESTCOOKING_API.Business.Services.IServices;
@@ -9,217 +10,239 @@ using NESTCOOKING_API.Utility;
 
 namespace NESTCOOKING_API.Business.Services
 {
-    public class UserService : IUserService
-    {
-        private readonly IUserRepository _userRepository;
-        private readonly IRoleRepository _roleRepository;
-        private readonly UserManager<User> _userManager;
-        private readonly IMapper _mapper;
-        private readonly ITransactionRepository _transactionRepository;
-        private readonly IRecipeRepository _recipeRepository;
+	public class UserService : IUserService
+	{
+		private readonly IUserRepository _userRepository;
+		private readonly IRoleRepository _roleRepository;
+		private readonly UserManager<User> _userManager;
+		private readonly IMapper _mapper;
+		private readonly ITransactionRepository _transactionRepository;
+		private readonly IRecipeRepository _recipeRepository;
 
-        public UserService(IUserRepository userRepository, IRoleRepository roleRepository, UserManager<User> userManager, IMapper mapper, ITransactionRepository transactionRepository, IRecipeRepository recipeRepository)
-        {
-            _userRepository = userRepository;
-            _roleRepository = roleRepository;
-            _userManager = userManager;
-            _mapper = mapper;
-            _transactionRepository = transactionRepository;
-            _recipeRepository = recipeRepository;
+		public UserService(IUserRepository userRepository, IRoleRepository roleRepository, UserManager<User> userManager, IMapper mapper, ITransactionRepository transactionRepository, IRecipeRepository recipeRepository)
+		{
+			_userRepository = userRepository;
+			_roleRepository = roleRepository;
+			_userManager = userManager;
+			_mapper = mapper;
+			_transactionRepository = transactionRepository;
+			_recipeRepository = recipeRepository;
+		}
 
-        }
+		public bool IsUniqueEmail(string email)
+		{
+			return this._userRepository.IsUniqueEmail(email);
+		}
+		public async Task<List<AdminUserDTO>> GetAllUsersAsync()
+		{
+			var userListFromDb = (await _userRepository.GetAllAsync()).ToList();
+			var userList = _mapper.Map<List<AdminUserDTO>>(userListFromDb);
+			for (int i = 0; i < userListFromDb.Count; i++)
+			{
+				var lockOutEnd = (await _userManager.GetLockoutEndDateAsync(userListFromDb[i])).GetValueOrDefault().LocalDateTime;
+				if (lockOutEnd >= DateTime.Now)
+					userList[i].LockOutEnd = lockOutEnd.ToString();
+			}
+			return userList;
+		}
 
-        public Task<User> GetUserByEmail(string email)
-        {
-            return _userManager.FindByEmailAsync(email);
-        }
+		public async Task LockUserAsync(string userId, int minute)
+		{
+			var userFromDb = await _userRepository.GetAsync(u => u.Id == userId);
+			if (await _userManager.IsLockedOutAsync(userFromDb))
+			{
+				var lockOutEnd = (await _userManager.GetLockoutEndDateAsync(userFromDb)).GetValueOrDefault().LocalDateTime;
+				throw new Exception($"This user has already been locked out, with the end time is {lockOutEnd.ToString()}.");
+			}
+			await _userManager.SetLockoutEndDateAsync(userFromDb, DateTime.Now.AddMinutes(minute));
+		}
+		public async Task UnlockUserAsync(string userId)
+		{
+			var userFromDb = await _userRepository.GetAsync(u => u.Id == userId);
+			if (!(await _userManager.IsLockedOutAsync(userFromDb)))
+			{
+				var lockOutEnd = (await _userManager.GetLockoutEndDateAsync(userFromDb)).GetValueOrDefault().LocalDateTime;
+				throw new Exception($"This user hasn't already been locked out..");
+			}
+			await _userManager.SetLockoutEndDateAsync(userFromDb, null);
+		}
 
-        public Task<User> GetUserByUsername(string username)
-        {
-            return _userManager.FindByNameAsync(username);
-        }
+		public async Task<UserDetailInfoDTO> GetUserById(string id)
+		{
+			var user = await _userManager.FindByIdAsync(id);
 
-        public bool IsUniqueEmail(string email)
-        {
-            return this._userRepository.IsUniqueEmail(email);
-        }
+			if (user == null)
+			{
+				throw new UserNotFoundException();
+			}
+			var userDTO = _mapper.Map<UserDetailInfoDTO>(user);
+			userDTO.Role = await _roleRepository.GetRoleNameByIdAsync(user.RoleId);
+			return userDTO;
+		}
 
-        public async Task<UserDetailInfoDTO> GetUserById(string id)
-        {
-            var user = await _userManager.FindByIdAsync(id);
+		public async Task<bool> ChangePassword(string userId, string currentPassword, string newPassword, string confirmPassword)
+		{
+			var user = await _userManager.FindByIdAsync(userId);
+			if (user == null)
+			{
+				return false;
+			}
 
-            if (user == null)
-            {
-                throw new UserNotFoundException();
-            }
-            var userDTO = _mapper.Map<UserDetailInfoDTO>(user);
-            userDTO.Role = await _roleRepository.GetRoleNameByIdAsync(user.RoleId);
-            return userDTO;
-        }
+			var result = await _userManager.ChangePasswordAsync(user,
+				currentPassword, newPassword);
 
-        public async Task<bool> ChangePassword(string userId, string currentPassword, string newPassword, string confirmPassword)
-        {
-            var user = await _userManager.FindByIdAsync(userId);
-            if (user == null)
-            {
-                return false;
-            }
+			if (!result.Succeeded)
+			{
+				return false;
+			}
+			return true;
+		}
 
-            var result = await _userManager.ChangePasswordAsync(user,
-                currentPassword, newPassword);
+		public async Task<UserDetailInfoDTO> EditProfile(string userId, UpdateUserDTO updateUserDTO)
+		{
+			var user = await _userManager.FindByIdAsync(userId);
+			if (user == null)
+			{
+				throw new UserNotFoundException();
+			}
 
-            if (!result.Succeeded)
-            {
-                return false;
-            }
-            return true;
-        }
+			user.Address = updateUserDTO.Address;
 
-        public async Task<UserDetailInfoDTO> EditProfile(string userId, UpdateUserDTO updateUserDTO)
-        {
-            var user = await _userManager.FindByIdAsync(userId);
-            if (user == null)
-            {
-                throw new UserNotFoundException();
-            }
+			user.IsMale = updateUserDTO.IsMale;
 
-            user.Address = updateUserDTO.Address;
+			user.UserName = updateUserDTO.UserName;
 
-            user.IsMale = updateUserDTO.IsMale;
+			user.FirstName = updateUserDTO.FirstName;
 
-            user.UserName = updateUserDTO.UserName;
+			user.LastName = updateUserDTO.LastName;
 
-            user.FirstName = updateUserDTO.FirstName;
+			user.AvatarUrl = updateUserDTO.AvatarUrl;
 
-            user.LastName = updateUserDTO.LastName;
+			var result = await _userManager.UpdateAsync(user);
+			if (!result.Succeeded)
+			{
+				throw new Exception();
+			}
 
-            user.AvatarUrl = updateUserDTO.AvatarUrl;
+			var userInfoDTO = await this.GetUserById(userId);
 
-            var result = await _userManager.UpdateAsync(user);
-            if (!result.Succeeded)
-            {
-                throw new Exception();
-            }
+			return userInfoDTO;
+		}
+		public async Task UpdateUserBalanceWithDeposit(string transactionId, double amount)
+		{
+			try
+			{
+				var transaction = await _transactionRepository.GetAsync(t => t.Id == transactionId);
+				var user = await _userManager.FindByIdAsync(transaction.UserId);
+				await UpdateUserBalance(user, amount);
+			}
+			catch (Exception ex)
+			{
+				throw new Exception("Error updating user balance.", ex);
+			}
+		}
+		public async Task UpdateUserBalanceWithPurchaseRecipe(double amount, string recipeId)
+		{
+			var recipe = await _recipeRepository.GetAsync(t => t.Id == recipeId);
+			var userRecipe = await _userManager.FindByIdAsync(recipe.UserId);
+			await UpdateUserBalance(userRecipe, amount * 0.9);
+			await ChangeAdminBalance(amount * 0.1);
+		}
 
-            var userInfoDTO = await this.GetUserById(userId);
+		public async Task<bool> ChangeUserBalanceByTranPurchased(string userId, double amount, string recipeId)
+		{
+			try
+			{
+				var user = await _userManager.FindByIdAsync(userId);
+				var recipe = await _recipeRepository.GetAsync(t => t.Id == recipeId);
+				var userCreatedRecipe = await _userManager.FindByIdAsync(recipe.UserId);
 
-            return userInfoDTO;
-        }
-        public async Task UpdateUserBalanceWithDeposit(string transactionId, double amount)
-        {
-            try
-            {
-                var transaction = await _transactionRepository.GetAsync(t => t.Id == transactionId);
-                var user = await _userManager.FindByIdAsync(transaction.UserId);
-                await UpdateUserBalance(user, amount);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("Error updating user balance.", ex);
-            }
-        }
-        public async Task UpdateUserBalanceWithPurchaseRecipe(double amount, string recipeId)
-        {
-            var recipe = await _recipeRepository.GetAsync(t => t.Id == recipeId);
-            var userRecipe = await _userManager.FindByIdAsync(recipe.UserId);
-            await UpdateUserBalance(userRecipe, amount * 0.9);
-            await ChangeAdminBalance(amount * 0.1);
-        }
+				// if (user.Balance < amount)
+				//     return false;
 
-        public async Task<bool> ChangeUserBalanceByTranPurchased(string userId, double amount, string recipeId)
-        {
-            try
-            {
-                var user = await _userManager.FindByIdAsync(userId);
-                var recipe = await _recipeRepository.GetAsync(t => t.Id == recipeId);
-                var userCreatedRecipe = await _userManager.FindByIdAsync(recipe.UserId);
+				// if (!await UpdateUserBalance(user, -amount))
+				//     return false;
+				// if (!await UpdateUserBalance(userCreatedRecipe, amount * 0.9))
+				// {
+				//     await UpdateUserBalance(user, amount);
+				//     return false;
+				// }
 
-                // if (user.Balance < amount)
-                //     return false;
+				// if (!await ChangeAdminBalance(amount * 0.1))
+				// {
+				//     await UpdateUserBalance(user, amount);
+				//     await UpdateUserBalance(userCreatedRecipe, -amount * 0.9);
+				//     return false;
+				// }
 
-                // if (!await UpdateUserBalance(user, -amount))
-                //     return false;
-                // if (!await UpdateUserBalance(userCreatedRecipe, amount * 0.9))
-                // {
-                //     await UpdateUserBalance(user, amount);
-                //     return false;
-                // }
+				// return true;
+				var updateUserBalanceResult = await UpdateUserBalance(user, -amount);
+				var updateUserRecipeBalanceResult = await UpdateUserBalance(userCreatedRecipe, amount * 0.9);
+				var changeAdminBalanceResult = await ChangeAdminBalance(amount * 0.1);
 
-                // if (!await ChangeAdminBalance(amount * 0.1))
-                // {
-                //     await UpdateUserBalance(user, amount);
-                //     await UpdateUserBalance(userCreatedRecipe, -amount * 0.9);
-                //     return false;
-                // }
+				if (updateUserBalanceResult && updateUserRecipeBalanceResult && changeAdminBalanceResult)
+				{
+					return true;
+				}
+				else
+				{
+					Console.WriteLine("One or more balance update operations failed.");
+					return false;
+				}
+			}
+			catch (Exception ex)
+			{
+				throw new Exception("Error changing user balance.", ex);
+			}
+		}
+		public async Task<bool> ChangeUserBalanceByWithdraw(string userId, double amount)
+		{
+			try
+			{
+				var user = await _userManager.FindByIdAsync(userId);
 
-                // return true;
-                var updateUserBalanceResult = await UpdateUserBalance(user, -amount);
-                var updateUserRecipeBalanceResult = await UpdateUserBalance(userCreatedRecipe, amount * 0.9);
-                var changeAdminBalanceResult = await ChangeAdminBalance(amount * 0.1);
+				if (user.Balance < amount)
+				{
+					throw new Exception("The amount of money in the wallet is not enough to make the transaction");
+				}
 
-                if (updateUserBalanceResult && updateUserRecipeBalanceResult && changeAdminBalanceResult)
-                {
-                    return true;
-                }
-                else
-                {
-                    Console.WriteLine("One or more balance update operations failed.");
-                    return false;
-                }
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("Error changing user balance.", ex);
-            }
-        }
-        public async Task<bool> ChangeUserBalanceByWithdraw(string userId, double amount)
-        {
-            try
-            {
-                var user = await _userManager.FindByIdAsync(userId);
+				if (!await UpdateUserBalance(user, -amount))
+					return false;
+				return true;
+			}
+			catch (Exception ex)
+			{
+				throw new Exception(ex.Message);
+			}
+		}
+		private async Task<bool> UpdateUserBalance(User user, double amount)
+		{
+			try
+			{
+				user.Balance += amount;
+				var result = await _userManager.UpdateAsync(user);
+				return result.Succeeded;
+			}
+			catch (Exception ex)
+			{
+				throw new Exception(ex.Message);
+			}
+		}
+		private async Task<bool> ChangeAdminBalance(double amount)
+		{
+			try
+			{
+				var roleAdminId = await _roleRepository.GetRoleIdByNameAsync(StaticDetails.Role_Admin);
+				var adminUser = await _userRepository.GetAsync(r => r.RoleId == roleAdminId);
+				if (adminUser == null)
+					return false;
 
-                if (user.Balance < amount)
-                {
-                    throw new Exception("The amount of money in the wallet is not enough to make the transaction");
-                }
-
-                if (!await UpdateUserBalance(user, -amount))
-                    return false;
-                return true;
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
-        private async Task<bool> UpdateUserBalance(User user, double amount)
-        {
-            try
-            {
-                user.Balance += amount;
-                var result = await _userManager.UpdateAsync(user);
-                return result.Succeeded;
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
-        private async Task<bool> ChangeAdminBalance(double amount)
-        {
-            try
-            {
-                var roleAdminId = await _roleRepository.GetRoleIdByNameAsync(StaticDetails.Role_Admin);
-                var adminUser = await _userRepository.GetAsync(r => r.RoleId == roleAdminId);
-                if (adminUser == null)
-                    return false;
-
-                return await UpdateUserBalance(adminUser, amount);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
-    }
+				return await UpdateUserBalance(adminUser, amount);
+			}
+			catch (Exception ex)
+			{
+				throw new Exception(ex.Message);
+			}
+		}
+	}
 }
